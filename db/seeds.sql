a3a8f81d3c8464651c884b1f04032f82b52c5bd3
PGCranford <PGCranford@gmail.com>
2022-07-18 18:34:02 -0400

    basics

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO EXISTS
+(id, first_name, last_name, role_id, manager_id)
+
+VALUES 
+('1','Richard', 'Nixon', '2', '3'),
+('2','Rube', 'Wadell', '2', '3'),
+('3','Anton', 'Lavey', '2', '3'),
+('4','Norman', 'Smiley', '2', '3'),
+('5','Dave', 'Anthony', '2', '3')
