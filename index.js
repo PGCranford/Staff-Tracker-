d5c7f7f1ed7af050d3e97d50e6310fa703723242
PGCranford <PGCranford@gmail.com>
2022-07-22 22:08:33 -0400

    working on create

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const Staff = {
     VIEW_DEPARTMENT: "View all departments",
     VIEW_ROLES: "View all roles",
     VIEW_EMPLOYEE: "View all employees",
+    ADD_DEPARTMENT: "Add a department",
+    ADD_EMPLOYEE: "Add and employee",
+    ADD_ROLE: "Add a role",
+    UPDATE_EMPLOYEE: "Update an employee role",
+
 };
 
 const viewStaff = async () => {
@@ -26,16 +31,25 @@ const start = async () => {
     while (!chooseExit) {
         const staff = await viewStaff();
         switch (staff) {
-            case viewStaff.VIEW_DEPARTMENT: {
+            case Staff.VIEW_DEPARTMENT: {
                 console.table(await orm.getDepartments());
                 break;
             }
-            case viewStaff.VIEW_EMPLOYEE: {
+            case Staff.VIEW_EMPLOYEE: {
                 console.table(await orm.getEmployee());
                 break;
             }
-            case viewStaff.VIEW_ROLES: {
+            case Staff.VIEW_ROLES: {
                 console.table(await orm.getRoles)
+                break;
+            }
+            case Staff.ADD_DEPARTMENT: {
+                const departmentName = await inquirer.prompt({
+                    type: "input",
+                    name: "departmentName",
+                    message: "What is the name of the new department?",
+                });
+                await orm.createDepartment(departmentName);
                 break;
             }
 
